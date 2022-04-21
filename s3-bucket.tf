743a67e7ea71d8ddfdaaeb6e07eab95514cdf1fd
Juan Maraboli <maraboli.juan@yahoo.com>
2022-04-21 20:12:23 +0000

    Added s3 bucket module

---
+++ s3-bucket.tf
@@ -0,0 +1,10 @@
+module "s3-bucket" {
+  source  = "app.terraform.io/training-workshop/s3-bucket/aws"
+  version = "2.8.0"
+  # insert required variables here
+  bucket_prefix = "juan-maraboli"
+}
+credentials "app.terraform.io" {
+    # valid user API token:
+    token = "xxxxxx.atlasv1.zzzzzzzzzzzzz"
+  }
